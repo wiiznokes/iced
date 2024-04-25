46b43746dbed3aea595d7c42892500c810ef606d
Ashley Wulber <ashley@system76.com>
2024-04-25 15:25:48 -0400

    fix(tiny_skia): damage

--- tiny_skia/src/window/compositor.rs
+++ tiny_skia/src/window/compositor.rs
@@ -183,31 +183,30 @@ pub fn present<T: AsRef<str>>(
         })
         .unwrap_or_else(|| vec![Rectangle::with_size(viewport.logical_size())]);
 
-    if damage.is_empty() {
-        return Ok(());
-    }
-
     surface.primitive_stack.push_front(primitives.to_vec());
-    surface.background_color = background_color;
 
-    let damage = damage::group(damage, scale_factor, physical_size);
+    if !damage.is_empty() {
+        surface.background_color = background_color;
 
-    let mut pixels = tiny_skia::PixmapMut::from_bytes(
-        bytemuck::cast_slice_mut(&mut buffer),
-        physical_size.width,
-        physical_size.height,
-    )
-    .expect("Create pixel map");
+        let damage = damage::group(damage, scale_factor, physical_size);
 
-    backend.draw(
-        &mut pixels,
-        &mut surface.clip_mask,
-        primitives,
-        viewport,
-        &damage,
-        background_color,
-        overlay,
-    );
+        let mut pixels = tiny_skia::PixmapMut::from_bytes(
+            bytemuck::cast_slice_mut(&mut buffer),
+            physical_size.width,
+            physical_size.height,
+        )
+        .expect("Create pixel map");
+
+        backend.draw(
+            &mut pixels,
+            &mut surface.clip_mask,
+            primitives,
+            viewport,
+            &damage,
+            background_color,
+            overlay,
+        );
+    }
 
     buffer.present().map_err(|_| compositor::SurfaceError::Lost)
 }
